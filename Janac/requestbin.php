906deb493bb1df9818ff951f1ea7ccc1f015a3aa
janacm <janac.meena@gmail.com>
2013-11-24 13:48:00 -0500

    great progress. We figured how to get json code and implement it with angular

---
+++ Janac/requestbin.php
@@ -0,0 +1,4 @@
+<?php
+    $result = file_get_contents('http://requestb.in/1go6iej1');
+    echo $result;
+?>
---
+++ postTest.php
@@ -0,0 +1,9 @@
+<html>
+  <body>
+    <form action="http://posttestserver.com/post.php?dir=example" method="post" enctype="text/plain">
+      Enter your task: <input type="text" name="submitted">
+      <input type="hidden" name="someParam" value="someValue"/>
+      <input type="submit" value="send">
+    </form>
+  </body>
+</html>	
